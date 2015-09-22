93c6b51383bce1c32417fbf0c4a1a8d2b30503e8
llych <llych@126.com>
2015-09-23 00:47:44 +0800

    修复小bug

--- app/views.py
+++ app/views.py
@@ -23,32 +23,25 @@ def weblog():
 
 @app.route('/index')
 def main():
-    d = datetime.datetime.now() - datetime.timedelta(days=7)
-    d_str = d.strftime('%Y-%m-%d')
+    interval_date = datetime.datetime.now() - datetime.timedelta(days=7)
+    interval_date_str = interval_date.strftime('%Y-%m-%d')
 
-    ms_data = total_log.query.filter(total_log.date > d_str).filter_by(
-        app_name='ms').order_by('date')
-    mi_data = total_log.query.filter(total_log.date > d_str).filter_by(
-        app_name='mi').order_by('date')
-    date_str = ','.join(["'%s'" % i.date for i in ms_data])
+    ms_data = total_log.query.filter(total_log.date > interval_date_str).filter_by(app_name='ms').order_by('date')
+    mi_data = total_log.query.filter(total_log.date > interval_date_str).filter_by(app_name='mi').order_by('date')
+    echarts_date = ','.join(["'%s'" % i.date for i in ms_data])
 
     ms_data_str = ','.join([str(i.total) for i in ms_data])
     mi_data_str = ','.join([str(i.total) for i in mi_data])
-    return render_template("ms_mi.html", date_str=date_str, ms_data_str=ms_data_str, mi_data_str=mi_data_str)
+    return render_template("ms_mi.html", echarts_date=echarts_date, ms_data_str=ms_data_str, mi_data_str=mi_data_str)
 
 
-@app.route('/select', methods=['POST'])
-def select():
-    print 'select'
-
-    select_date = request.form.get('date', '')
-    print select_date, 'fff'
-    if select_date != '':
-        print 'xxxx'
-        ms_mi_log_data = ms_mi_log.query.filter(
-            ms_mi_log.date.like(select_date+'%')).order_by('ip').all()
-        for s in ms_mi_log_data:
-            print s
+@app.route('/query', methods=['POST'])
+def query():
+    query_date = request.form.get('date', '')
+    if query_date != '':
+        ms_mi_log_data = ms_mi_log.query.filter(ms_mi_log.date.like(query_date+'%')).order_by('ip').all()
+        # for s in ms_mi_log_data:
+        #     print s
     else:
         ms_mi_log_data = []
 
@@ -70,26 +63,20 @@ def api():
 
 
 def event_stream(ip, logfile, filter=None):
-    logfile = os.path.join('/home/opes/logs',logfile)
-    if filter is not None:
-        re_filter=re.compile(r'(%s)'%filter,re.I)
+    logfile = os.path.join('/logs/log_',logfile)
     with open(logfile) as f:
             f.seek(0,2)
             while True:
-                #print 'OK'
-                #print f.tell()
-
                 for line in f.readlines():
                     if filter is not None:
+                        re_filter=re.compile(r'(%s)'%filter,re.I)
                         if re.findall(re_filter,line):
                             res=re.sub(re_filter,r'<font color="red">\1</font>',line)
                             yield 'data: %s\n\n' % res.rstrip()
                     else:
                         yield 'data: %s\n\n' % line.rstrip()
                 time.sleep(1)
-    # for i in xrange(1, 10000):
-    #     yield 'data: %s\n\n' % i
-    #     time.sleep(1)
+
 
 @app.route('/stream/<ip>/<logfile>/')
 @app.route('/stream/<ip>/<logfile>/<filter>')
